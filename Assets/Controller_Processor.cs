a874298e6c3543b4f5055f27ef1ca0dd570c555c
Park, Jonghyeon <belline0124@gmail.com>
2024-03-27 10:28:58 +0900

    Add Processor moving automatically

--- Assets/Controller_Processor.cs
+++ Assets/Controller_Processor.cs
@@ -3,42 +3,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-public class ProcessorHeading
-{
-    public Vector2 src;
-    public Vector2 Src { get { return src; } }
-    public Vector2 dest;
-    public Vector2 Dest { get { return dest; } }
-    public Vector2 dir;
-    public Vector2 Dir { get { return dir; } }
-    public ProcessorHeading(Vector2 src, Vector2 dest)
-    {
-        this.src = src;
-        this.dest = dest;
-        this.dir = this.Dest - this.Src;
-    }
-}
 
 public class Controller_Processor : MonoBehaviour
 {
     public Config_Processor config = new Config_Processor();
     public List<PositionPoint> pointsHeading = new List<PositionPoint>();
     public List<PositionPoint> pointsPassed = new List<PositionPoint>();
-    public ProcessorHeading processorHeading;
     public MovingStatus movingStatus = MovingStatus.Idle;
-
+    public Vector2 processorHeading;
     public GameObject displayModelObject;
 
     void Awake()
     {
-        this.displayModelObject = transform.Find(config.modelObjectName).gameObject;
+        this.displayModelObject = transform.Find(this.config.modelObjectName).gameObject;
     }
 
     void Start()
     {
-        TargetingNextPosition();
-        this.movingStatus = MovingStatus.Rotating;
+        SetTarget();
     }
 
     void Update()
@@ -48,29 +30,75 @@ public class Controller_Processor : MonoBehaviour
 
     void ProcessingMoving()
     {
+        if (this.movingStatus == MovingStatus.Idle) return;
+
         float step;
-        switch (movingStatus)
+        switch (this.movingStatus)
         {
-            case MovingStatus.Forwading:
-                step = config.velocity * Time.deltaTime;/*
-                Vector3 newPosition = Vector3.MoveTowardstr(transform.position, 
-                    new Vector3()
-                );
-                transform.position = Vector3;*/
-            break;
+            case MovingStatus.Forwarding:
+                if (Vector3.Distance(transform.position, VectorUtils.Vector2To3(processorHeading, transform.position.y)) < .001f)
+                {
+                    SwitchTarget();
+                }
+                else
+                {
+                    _ProcessingForwarding();
+                }
+                break;
             case MovingStatus.Rotating:
+                _ProcessingRotating();
+                /*
                 step = config.angularVelocity * Time.deltaTime;
                 Vector3 newDirection = Vector3.RotateTowards(transform.forward, processorHeading.Dir, step, 0f);
                 transform.rotation = Quaternion.LookRotation(newDirection);
+                */
             break;
         }
     }
-
-    void TargetingNextPosition()
+    void SetTarget()
     {
-        processorHeading = new ProcessorHeading(
-            new Vector2(transform.position.x, transform.position.y),
-            new Vector2(pointsHeading[0].position.x, pointsHeading[0].position.y)
+        if (this.pointsHeading.Count == 0) return;
+        this.processorHeading = this.pointsHeading[0].position;
+        _SetMovingStatusToStart();
+    }
+    void _SetMovingStatusToStart()
+    {
+        this.movingStatus = MovingStatus.Rotating;
+    }
+    void SwitchTarget()
+    {
+        if (pointsHeading.Count == 0)
+        {
+            this.movingStatus = MovingStatus.Idle;
+            return;
+        }
+        this.pointsPassed.Add(this.pointsHeading[0]);
+        this.pointsHeading.RemoveAt(0);
+        SetTarget();
+        // check angular
+    }
+    void _ProcessingForwarding()
+    {
+        float step = this.config.velocity * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, VectorUtils.Vector2To3(processorHeading, transform.position.y), step);
+
+        /*
+        Vector3 newPosition = Vector3.MoveTowardstr(transform.position, 
+            new Vector3()
         );
+        transform.position = Vector3;*/
+    }
+    void _ProcessingRotating()
+    {
+        Vector3 targetDirection = VectorUtils.Vector2To3(processorHeading, transform.position.y) - transform.position;
+        Quaternion targetDirectionQ = Quaternion.LookRotation(targetDirection);
+        float step = this.config.angularVelocity * Time.deltaTime;
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, step, 0f);
+        transform.rotation = Quaternion.LookRotation(newDirection);
+
+        if (targetDirectionQ == transform.rotation)
+        {
+            this.movingStatus = MovingStatus.Forwarding;
+        }
     }
 }
--- Assets/Scripts/Definitions/Classes_Common.cs
+++ Assets/Scripts/Definitions/Classes_Common.cs
@@ -35,3 +35,11 @@ public class Config_Rigidbody
         rb.isKinematic = this.isKinematic;
     }
 }
+
+public class VectorUtils
+{
+    public static Vector3 Vector2To3(Vector2 vector, float y = 0)
+    {
+        return new Vector3(vector.x, y, vector.y);
+    }
+}
